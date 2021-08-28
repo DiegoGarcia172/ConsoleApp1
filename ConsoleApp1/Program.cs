d41a0708a808f89d3bcb011011bf1f6203b0ed34
Diego Garcia <Diego Garcia@DESKTOP-IB4I1ME>
2021-08-27 21:47:43 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe tu nombre: ");
+            string nombre = Convert.ToString(Console.ReadLine());
+
+
+            Console.WriteLine("Escribe tu primer numero: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Escribe tu segundo numero:  ");
+            Double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double resultado;
+
+            resultado = num1 + num2; 
+      
+            Console.WriteLine("Tu resultado "+ nombre +" es" +  resultado);
+
+            Console.ReadLine();
+
+
+
+        }
+
+    }
+
+}
